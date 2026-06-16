edac025436de97cf41079ddf0bc3f3a28fb958d1
Ryzn.Dev <dev.ryzn@gmail.com>
2026-06-16 20:40:04 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'horizon-id-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
